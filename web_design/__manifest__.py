e61145e868f1cc947a84f842bdf4694a010b0b0d
Aboobacker Bilal <aboobackerbilalp@gmail.com>
2023-12-20 11:29:56 +0530

    Odoo web design

---
+++ web_design/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': "web design",
+    'version':'16.0.2',
+    'summary': 'web design software',
+    'depends':['website','base'],
+    'data': [
+        'views/web_design.xml'
+    ],
+    'instalable': True,
+    'application': True,
+}
